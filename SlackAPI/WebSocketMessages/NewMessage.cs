5e9052846bd72e6853eba8b0995873acff1766d9
Tom Postler <Tom.Postler@microsoft.com>
2017-02-01 09:28:12 -0600

    channel_join

--- SlackAPI/WebSocketMessages/NewMessage.cs
+++ SlackAPI/WebSocketMessages/NewMessage.cs
@@ -4,6 +4,7 @@ namespace SlackAPI.WebSocketMessages
 {
     [SlackSocketRouting("message")]
     [SlackSocketRouting("message", "bot_message")]
+    [SlackSocketRouting("message", "channel_join")]
     public class NewMessage : SlackSocketMessage
     {
         public string user;
